268caf7688212f7ee5d8c562dc8382fa350b868e
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2020-02-26 14:22:46 -0500

    'heroku'

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const cors = require('cors');
 const app = express();
+const path = require('path');
 // Bodyparser middleware
 
 app.use(cors());
@@ -30,5 +31,14 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
